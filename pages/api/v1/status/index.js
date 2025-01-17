24499afc9a88b2ae9380d878a5df6fa5ab5350f8
Marcelo Neves Lira <78197411+mandicneves@users.noreply.github.com>
2025-01-17 14:44:54 +0000

    adiciona os arquivos `database.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,9 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "mensagem usando o json" });
-  // response.status(200).send("Mensagem usando o send");
 }
 
 export default status;
